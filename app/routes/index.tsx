42d39fc1e67bba69ae45357d62afd83fbc61bc8d
D&nny#3692 <daniel.pilkington@outlook.com>
2022-05-25 09:32:38 +0100

    Update index.tsx
    removed
    <img
                      src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
                      alt="Remix"
                      className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
                    />

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -51,11 +51,6 @@ export default function Index() {
                 )}
               </div>
               <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
               </a>
             </div>
           </div>
